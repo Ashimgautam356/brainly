54ecdda549c03f212d78c955a39e0d4c3c48ae0f
Ashim Gautam <computer77356@gmail.com>
2024-12-03 23:19:55 +0545

    working in userAuth

--- backend/src/middleware/userAuth.ts
+++ backend/src/middleware/userAuth.ts
@@ -1,29 +1,29 @@
-// import { Request,Response,NextFunction } from "express"
-// import { z} from 'zod'
-// import bcrypt from 'bcrypt'
-// import { userModel } from "../db"
-// import jwt from 'jsonwebtoken'
+import { Request,Response,NextFunction } from "express"
+import { z} from 'zod'
+import bcrypt from 'bcrypt'
+import { userModel } from "../db"
+import jwt from 'jsonwebtoken'
 
-// export const userAuth = async(req:Request,res:Response,next:NextFunction)=>{
-//     const userToken = req.headers.token;
+export const userAuth = async(req:Request,res:Response,next:NextFunction)=>{
+    const userToken = req.headers.token;
 
-//     if(!userToken){
-//         res.status(404).json({
-//             "message":"token not foudn"
-//         })
-//         return;
-//     }
-//     const isValidToken = jwt.verify(String(userToken),"this is secrete")
+    if(!userToken){
+        res.status(404).json({
+            "message":"token not foudn"
+        })
+        return;
+    }
+    const isValidToken = jwt.verify(String(userToken),"this is secrete")
 
-//     if(!isValidToken){
-//         res.status(404).json({
-//             "message":"invalid user"
-//         })
-//         return;
-//     }
-//     const userId = jwt.decode(String(userToken))?.id
-//     const user = await userModel.findById(userId)
-//     req.body.user = user;
-//     next()
+    if(!isValidToken){
+        res.status(404).json({
+            "message":"invalid user"
+        })
+        return;
+    }
+    const userId = jwt.decode(String(userToken))?.id
+    const user = await userModel.findById(userId)
+    req.body.user = user;
+    next()
 
-// }
+}
