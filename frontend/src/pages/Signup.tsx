d249c94f5b1c45bfe7946fee210002cb80c9d4b0
Ashim Gautam <computer77356@gmail.com>
2024-12-08 13:43:36 +0545

    improved design in signup Page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -4,7 +4,7 @@ import { Button } from '../components/Button'
 import axios from 'axios'
 import { BACKEND_URL } from '../Config'
 import { useNavigate } from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
 
 export const Signup = () => {
     const userNameRef = React.useRef<HTMLInputElement>()
@@ -24,12 +24,21 @@ export const Signup = () => {
     }
   return (
     <div className='flex justify-center items-center bg-gray-200 w-screen h-screen'>
-        <div className='bg-white min-w-48 rounded border p-8'>
-            <Input reference={userNameRef} placeholder='UserName'></Input>
-            <Input reference={passwordRef} placeholder='passwordRef'></Input>
+        <div className='bg-white min-w-48 rounded-lg border p-8 h-80 flex flex-col justify-between items-center'>
+            <div className='flex justify-center '>
+                <h1 className='text-3xl font-semibold text-purple-600'>Signup</h1>
+            </div>
+            <div className=''>
 
-            <div className='flex justify-center mt-4'>
+                <Input reference={userNameRef} placeholder='UserName'></Input>
+                <Input reference={passwordRef} placeholder='password'></Input>
+            </div>
+
+            <div className='flex justify-center w-full'>
                 <Button variants='primary' text='Signup' size='md' fullWidth={true} loading={false} onClick={signup}></Button>
+            </div>
+            <div className=''>
+                <p>Have an Account?<span><Link to={'/'} className='underline text-blue-500'>Login</Link></span></p>
             </div>
             
         </div>
